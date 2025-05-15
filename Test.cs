338c5053cacf08ec81c090d76aab02b83fbcdb51
Venu Koneru <v-venukoneru@microsoft.com>
2025-05-15 15:23:53 -0500

    Update Class1 constructor output messages
    Modified the constructor of `Class1` to replace the previous
    output messages with new ones. The old messages ("Hello World 1",
    "Hello World 2", and "Hello World FeatureBranch Added") have been
    removed and replaced with "Hello World FeatureBranch Added commit 1"
    and "Hello World FeatureBranch Added commit 2".

--- Test.cs
+++ Test.cs
@@ -3,9 +3,8 @@
 public class Class1
 {
 	public Class1()
-	{
-        Console.WriteLine("Hello World 1");
-        Console.WriteLine("Hello World 2");
-        Console.WriteLine("Hello World FeatureBranch Added");
+	{       
+        Console.WriteLine("Hello World FeatureBranch Added commit 1");
+        Console.WriteLine("Hello World FeatureBranch Added commit 2");
     }
 }
