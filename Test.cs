bef136666bd38a2782e0bb9d8631e620a6bc1f8d
Venu Koneru <v-venukoneru@microsoft.com>
2025-05-15 15:31:41 -0500

    Add console output to Class1 constructor
    Updated the constructor of the `Class1` class to include multiple `Console.WriteLine` statements. The new output includes "Main Commit 1" and three additional messages related to commits made in the "FeatureBranch".

--- Test.cs
+++ Test.cs
@@ -3,7 +3,8 @@
 public class Class1
 {
 	public Class1()
-	{       
+	{   
+        Console.WriteLine("Main Commit 1");
         Console.WriteLine("Hello World FeatureBranch Added commit 1");
         Console.WriteLine("Hello World FeatureBranch Added commit 2");
         Console.WriteLine("Hello World FeatureBranch Added commit 3");
