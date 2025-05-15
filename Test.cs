b70c623d7c8a3f9eee9ce028a32e8629aed067ce
Venu Koneru <v-venukoneru@microsoft.com>
2025-05-15 16:58:09 -0500

    Add console output for local branch commit
    Added a new line in `Class1` of `Test.cs` to output
    the message "Hello World FeatureBranch Added Rebase
    commit Local Branch 1" to the console, enhancing
    logging information alongside existing outputs.

--- Test.cs
+++ Test.cs
@@ -25,5 +25,7 @@ public class Class1
         Console.WriteLine("Hello World FeatureBranch Added Rebase commit 22");
 
         Console.WriteLine("Hello World FeatureBranch Added Rebase commit 23");
+
+        Console.WriteLine("Hello World FeatureBranch Added Rebase commit Local Branch 1");
     }
 }
