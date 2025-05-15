4c5c304710dab7bbe0e91b484c6d32d357aa7d05
Venu Koneru <v-venukoneru@microsoft.com>
2025-05-15 15:38:01 -0500

    Add console output for feature branch commits
    Updated `Test.cs` to include a new console output line: "Hello World FeatureBranch Added commit 1" in the `Class1` constructor. Noted potential duplication of "Hello World FeatureBranch Added commit 4" in the existing output lines.

--- Test.cs
+++ Test.cs
@@ -5,6 +5,7 @@ public class Class1
 	public Class1()
 	{   
         Console.WriteLine("Main Commit 1");
+        Console.WriteLine("Hello World FeatureBranch Added commit 1");
         Console.WriteLine("Main Commit 2");
         Console.WriteLine("Hello World FeatureBranch Added commit 4");
         Console.WriteLine("Hello World FeatureBranch Added commit 2");
